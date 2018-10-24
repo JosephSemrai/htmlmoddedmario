56129dc79a4b7b98043eb9086c006500699fea33
Joseph Semrai <josephsemrai@gmail.com>
2018-10-23 23:47:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿var time = Date.now();
+
+document.onreadystatechange = function (event) {
+    if (event.target.readyState !== "complete") {
+        return;
+    }
+
+    var UserWrapper = new UserWrappr.UserWrappr(FullScreenMario.FullScreenMario.prototype.proliferate(
+        {
+            "GameStartrConstructor": FullScreenMario.FullScreenMario
+        }, FullScreenMario.FullScreenMario.settings.ui, true));
+
+    console.log("u r " + (Date.now() - time) + " milliseconds late.");
+    
+    UserWrapper.GameStarter.UsageHelper.displayHelpMenu();
+};
